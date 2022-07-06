d57d329cd799bc94221b628f8fc09c66d59ba348
Husainbw786 <89205542+Husainbw786@users.noreply.github.com>
2022-07-06 04:23:25 -0700

    Create C++ STL | Set 5 (queue) .cpp

---
+++ Queue/C++ STL | Set 5 (queue) .cpp
@@ -0,0 +1,54 @@
+void push(queue<int> &q,int x)
+{
+    //Your code here
+    q.push(x);
+}
+
+
+/*pop out the front element 
+from the queue q and returns it */
+int pop(queue<int> &q)
+{
+   //Your code here
+  
+   while(!q.empty())
+   {
+       int x = q.front();
+       q.pop();
+       return x;
+   }
+   return -1;
+}
+
+/*returns the size of the queue q */
+int getSize(queue<int> &q)
+{
+    //Your code here
+   
+   return q.size();
+    
+}
+
+/*returns the last element of the queue */
+int getBack(queue<int> &q)
+{
+	//Your code here
+	if(!q.empty())
+	{
+	int x = q.back();
+	return x ;
+	}
+	return -1;
+}
+
+/*returns the first element of the queue */
+int getFront(queue<int> &q)
+{
+	//Your code here
+  if(!q.empty())
+	{
+	int x = q.front();
+	return x ;
+	}
+	return -1;
+}
