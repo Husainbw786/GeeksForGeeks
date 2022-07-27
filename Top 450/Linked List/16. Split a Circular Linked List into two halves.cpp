1eceb1c5c471ece3446dbe5628bee428056332e1
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-07-27 06:25:20 -0700

    Create 16. Split a Circular Linked List into two halves.cpp

---
+++ Top 450/Linked List/16. Split a Circular Linked List into two halves.cpp
@@ -0,0 +1,27 @@
+void splitList(Node *head, Node **head1_ref, Node **head2_ref)
+{
+    // your code goes here
+    if( head == NULL || head->next == NULL)
+    {
+        return;
+    }
+    Node* slow = head;
+    Node* fast = head->next;
+    while(fast != head && fast->next != head)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    Node* aftermid = slow->next;
+    *head1_ref = head;
+    slow->next = head;
+    *head2_ref = aftermid;
+    Node* temp = *head2_ref;
+    while(temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = *head2_ref;
+    
+    
+    
