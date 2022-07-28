19e9e13932c1724144cf051afb042b73c2f41d9b
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-07-28 06:22:05 -0700

    Create Delete nodes having greater value on right .cpp

---
+++ Top 450/Linked List/Delete nodes having greater value on right .cpp
@@ -0,0 +1,27 @@
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        // your code goes here
+        Node* Curr = head;
+        Node* temp =  Curr;
+        while(Curr->next != NULL)
+        {
+            if(Curr->data < Curr->next->data)
+            {
+                temp = Curr;
+                temp->data = temp->next->data;
+                temp->next = temp->next->next;
+                Curr = head;
+                
+            }
+            else
+            {
+                Curr = Curr->next;
+            }
+        }
+        return head;
+    }
+    
+};
