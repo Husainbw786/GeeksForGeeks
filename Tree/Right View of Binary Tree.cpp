7d3864d34e98181b429bbdb2513aabd2bb64936e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-07-30 19:46:28 -0700

    Create Right View of Binary Tree.cpp

---
+++ Tree/Right View of Binary Tree.cpp
@@ -0,0 +1,35 @@
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+       // Your Code here
+       deque<Node*>q;
+       vector<int>v;
+       if(!root)
+       {
+           return v;
+       }
+       q.push_front(root);
+       while(!q.empty())
+       {
+           v.push_back(q.back()->data);
+           int size = q.size();
+           while(size--)
+           {
+               Node* temp = q.front();
+               q.pop_front();
+               if(temp->left)
+               {
+                   q.push_back(temp->left);
+               }
+               if(temp->right)
+               {
+                   q.push_back(temp->right);
+               }
+           }
+       }
+       return v;
+    }
+};
