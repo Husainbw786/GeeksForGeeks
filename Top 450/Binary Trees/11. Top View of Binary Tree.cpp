ba1073372a8b5c416123ad35088221638755058e
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-08 08:19:10 -0700

    Create 11. Top View of Binary Tree.cpp

---
+++ Top 450/Binary Trees/11. Top View of Binary Tree.cpp
@@ -0,0 +1,41 @@
+class Solution
+{
+    public:
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    vector<int> topView(Node *root)
+    {
+        //Your code here
+        map<int,int>mp;
+        vector<int>v;
+        if(root == NULL)
+        {
+            return v;
+        }
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        while(!q.empty())
+        {
+            Node* temp = q.front().first;
+            int height = q.front().second;
+            q.pop();
+            if(mp[height] == 0)
+            {
+                mp[height] = temp->data;
+            }
+            if(temp->left)
+            {
+                q.push({temp->left,height-1});
+            }
+            if(temp->right)
+            {
+                q.push({temp->right,height+1});
+            }
+        }
+        for( auto x:mp)
+        {
+            cout << x.second << " ";
+        }
+    }
+
+};
