f282ef80934230d5a7831243fb3e6aed1df9abb3
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-08 20:49:24 -0700

    Create 16.  Boundary Traversal of binary tree.cpp

---
+++ Top 450/Binary Trees/16.  Boundary Traversal of binary tree.cpp
@@ -0,0 +1,81 @@
+class Solution {
+public:
+     
+     bool isLeaf(Node* root)
+     {
+         if(root->left || root->right)
+         {
+             return false;
+         }
+         else
+         {
+             return true;
+         }
+     }
+    
+    void LeftTree(Node*root, vector<int>&v)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        if(root->left)
+        {
+            v.push_back(root->data);
+            LeftTree(root->left,v);
+        }
+        else if(root->right)
+        {
+            v.push_back(root->data);
+            LeftTree(root->right,v);
+        }
+    }
+    void Leaf(Node* root, vector<int>&v)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+        Leaf(root->left,v);
+        if(root->left == NULL && root->right == NULL)
+        {
+            v.push_back(root->data);
+        }
+        Leaf(root->right,v);
+    }
+    void RightTree(Node* root, vector<int>&v)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+       if(root->right)
+       {
+           RightTree(root->right,v);
+           v.push_back(root->data);
+       }
+       else if(root->left)
+       {
+           RightTree(root->left,v);
+           v.push_back(root->data);
+       }
+    }
+
+    vector <int> boundary(Node *root)
+    {
+        //Your code here
+        vector<int>v;
+        if(root == NULL)
+        {
+            return v;
+        }
+        if(!isLeaf(root))
+        {
+            v.push_back(root->data);
+        }
+        LeftTree(root->left,v);
+        Leaf(root,v);
+        RightTree(root->right,v);
+        return v;
+    }
+};
