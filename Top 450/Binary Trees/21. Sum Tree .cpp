ee43c19b10528ad020dce5aaa05760d9b308e48f
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-08 21:51:38 -0700

    Create 21. Sum Tree .cpp

---
+++ Top 450/Binary Trees/21. Sum Tree .cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+    public:
+    int to_check(Node* root, bool&flag)
+    {
+        if(root == NULL)
+        {
+            return 0;
+        }
+        int left = to_check(root->left,flag);
+        int right = to_check(root->right,flag);
+     if(!(root->left==NULL && root->right==NULL) && (root->data != left+right) )
+     {
+         flag = 0;
+     }
+     return left+right+root->data;
+         
+    }
+    bool isSumTree(Node* root)
+    {
+         // Your code here
+         bool flag = 1;
+         to_check(root,flag);
+         return flag;
+    }
+};
