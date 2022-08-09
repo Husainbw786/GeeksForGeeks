8021ba70bdf106d93106bd34e6209dc1e770cdda
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-08 22:03:35 -0700

    Create 23. Leaf at same level.cpp

---
+++ Top 450/Binary Trees/23. Leaf at same level.cpp
@@ -0,0 +1,43 @@
+
+class Solution{
+  public:
+    /*You are required to complete this method*/
+    bool check(Node *root)
+    {
+        //Your code here
+        int flag = 0;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            int size = q.size();
+            for(int i=0;i<size;i++)
+            {
+                Node* Curr = q.front();
+                q.pop();
+                if(Curr->left == NULL && Curr->right == NULL)
+                {
+                    flag = 1;
+                }
+                if(Curr->left)
+                {
+                    q.push(Curr->left);
+                }
+                if(Curr->right)
+                {
+                    q.push(Curr->right);
+                }
+            }
+            if(flag == 1 && q.empty())
+             {
+               return true;
+             }
+            if(flag == 1 && !q.empty())
+             {
+               return false;
+             }
+            
+        }
+        
+    }
+};
