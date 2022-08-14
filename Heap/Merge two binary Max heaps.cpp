257ca9e3d6ec888737c9f042a694fdf902ae6e16
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-14 06:13:25 -0700

    Create Merge two binary Max heaps.cpp

---
+++ Heap/Merge two binary Max heaps.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    vector<int> mergeHeaps(vector<int> &a, vector<int> &b, int n, int m) {
+        // your code here
+        priority_queue<int>pq;
+        int i;
+        vector<int>v;
+        for(i=0;i<n;i++)
+        {
+            pq.push(a[i]);
+        }
+        for(i=0;i<m;i++)
+        {
+            pq.push(b[i]);
+        }
+        for(i=0;i<n+m;i++)
+        {
+            v.push_back(pq.top());
+            pq.pop();
+        }
+        return v;
+    }
+};
