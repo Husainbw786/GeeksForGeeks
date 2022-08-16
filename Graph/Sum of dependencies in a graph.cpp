c63132d01bc637401210a2a85a25619a399471b5
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-16 06:00:25 -0700

    Create Sum of dependencies in a graph.cpp

---
+++ Graph/Sum of dependencies in a graph.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    int sumOfDependencies(vector<int> adj[], int V) {
+        // code here
+        int count = 0;
+        for(int i=0;i<V;i++)
+        {
+            count += adj[i].size();
+            
+        }
+        return count;
+    }
+};
