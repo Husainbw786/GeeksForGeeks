cc4a30c51b47575e451bb1a0f3befcf33d608c8b
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-08-21 20:20:32 -0700

    Create Knight Walk.cpp

---
+++ Graph/Knight Walk.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int N){
+	    // Code here
+	    int mat[N][N];
+	    int x1 = KnightPos[0];
+	    int y1 = KnightPos[1];
+	    int x2 = TargetPos[0];
+	    int y2 = TargetPos[1];
+	    if(x1 == x2 && y1 == y2)
+	    {
+	        return 0;
+	    }
+	    queue<pair<int,int>>q;
+	    int i,j;
+	    for(i=0;i<N;i++)
+	    {
+	        for(j=0;j<N;j++)
+	        {
+	            mat[i][j] = 0;
+	        }
+	    }
+	    q.push({x1-1,y1-1});
+	    while(!q.empty())
+	    {
+	        auto p = q.front();
+	        int i = p.first;
+	        int j = p.second;
+	        q.pop();
+	        
+	        if( (i+1)>= 0 && (i+1) < N && (j+2) >= 0 && (j+2) < N && mat[i+1][j+2] == 0)
+	        {
+	            mat[i+1][j+2] = mat[i][j] + 1;
+	            q.push({i+1,j+2});
+	        }
+	        
+	        if( (i+2)>= 0 && (i+2) < N && (j+1) >= 0 && (j+1) < N && mat[i+2][j+1] == 0)
+	        {
+	            mat[i+2][j+1] = mat[i][j] + 1;
+	            q.push({i+2,j+1});
+	        }
+	        if( (i-1)>= 0 && (i-1) < N && (j+2) >= 0 && (j+2) < N && mat[i-1][j+2] == 0)
+	        {
+	            mat[i-1][j+2] = mat[i][j] + 1;
+	            q.push({i-1,j+2});
+	        }
+	        if( (i-2)>= 0 && (i-2) < N && (j+1) >= 0 && (j+1) < N && mat[i-2][j+1] == 0)
+	        {
+	            mat[i-2][j+1] = mat[i][j] + 1;
+	            q.push({i-2,j+1});
+	        }
+	        if( (i+2)>= 0 && (i+2) < N && (j-1) >= 0 && (j-1) < N && mat[i+2][j-1] == 0)
+	        {
+	            mat[i+2][j-1] = mat[i][j] + 1;
+	            q.push({i+2,j-1});
+	        }
+	        if( (i+1) >= 0 && (i+1) < N && (j-2) >= 0 && (j-2) < N && mat[i+1][j-2] == 0)
+	        {
+	            mat[i+1][j-2] = mat[i][j] + 1;
+	            q.push({i+1,j-2});
+	        }
+	        if( (i-1)>= 0 && (i-1) < N && (j-2) >= 0 && (j-2) < N && mat[i-1][j-2] == 0)
+	        {
+	            mat[i-1][j-2] = mat[i][j] + 1;
+	            q.push({i-1,j-2});
+	        }
+            if( (i-2)>= 0 && (i-2) < N && (j-1) >= 0 && (j-1) < N && mat[i-2][j-1] == 0)
+	        {
+	            mat[i-2][j-1] = mat[i][j] + 1;
+	            q.push({i-2,j-1});
+	        }
+	        
+	    }
+	    return mat[x2-1][y2-1];
+	}
+};
