cf901aab923533b111ef2313b0ad068090011e6d
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2022-09-10 18:24:50 +0530

    Create Rearrange an array with O(1) extra space.cpp

---
+++ array/Rearrange an array with O(1) extra space.cpp
@@ -0,0 +1,22 @@
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    //with O(1) extra space.
+    void arrange(long long arr[], int n) {
+        // Your code here
+        
+        for(int i=0;i<n;i++)
+        {
+        
+        int x = arr[i];
+        int y = arr[x];
+        arr[i] = x + (y % n)*n;
+        }
+        for(int i=0;i<n;i++)
+        {
+            arr[i] = arr[i] / n;
+        }
+    }
+};
