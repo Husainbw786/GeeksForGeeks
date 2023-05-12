6b5ad395cc85e886d400543119d59d9e081bc1ae
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-05-11 20:31:32 -0700

    Added Solution - GfG to GitHub

---
+++ Easy/Array Operations/array-operations.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int arrayOperations(int n, vector<int> &arr) {
+        // code here
+        int i;
+        int ans = 0;
+        int digits = 0;
+        for(i=0;i<n;i++)
+        {
+            if(arr[i] == 0)
+            {
+               if(digits > 0)
+               {
+                   ans++;
+               }
+               digits = 0;
+            }
+            else
+            {
+                digits++;
+            }
+        }
+        if(digits > 0)
+        {
+            ans++;
+        }
+        
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        
+        int n;
+        cin>>n;
+        
+        
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        
+        Solution obj;
+        int res = obj.arrayOperations(n, arr);
+        
+        cout<<res<<endl;
+        
+    }
+}
+
+// } Driver Code Ends
