79cac69f11b9848be4407bf85039f4baf0a0b8e4
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-05-20 11:04:22 -0700

    Added Solution - GfG to GitHub

---
+++ Hard/Find number of closed islands/find-number-of-closed-islands.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    public:
+    
+    
+    void dfs(int i, int j, vector<vector<int>>&matrix)
+    {
+        if(i < 0 || j < 0 || i >= matrix.size() || j >= matrix[0].size() || matrix[i][j] == 0)
+        {
+            return;
+        }
+        matrix[i][j] = 0;
+        dfs(i+1,j,matrix);
+        dfs(i-1,j,matrix);
+        dfs(i,j+1,matrix);
+        dfs(i,j-1,matrix);
+    }
+    
+    
+    int closedIslands(vector<vector<int>>& matrix, int N, int M) {
+        // Code here
+        int i,j;
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if((i == 0 || j == 0 || i == N-1 || j == M-1) && matrix[i][j] == 1)
+                {
+                    dfs(i,j,matrix);
+                }
+            }
+        }
+        int count = 0;
+        for(i=0;i<N;i++)
+        {
+            for(j=0;j<M;j++)
+            {
+                if(matrix[i][j] == 1)
+                {
+                    count++;
+                    dfs(i,j,matrix);
+                    
+                }
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+        int N, M;
+        cin >> N >> M;
+        vector<vector<int>>matrix(N, vector<int>(M, 0));
+        for(int i=0; i<N; i++)
+            for(int j=0; j<M; j++)
+                cin >> matrix[i][j];
+                
+        Solution obj;
+        int ans = obj.closedIslands(matrix, N, M);
+        cout << ans << "\n";
+    }
+	
+	return 0;
+	
+}
+
+
+// } Driver Code Ends
