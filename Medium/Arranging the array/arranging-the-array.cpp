81cf1019c123587a7a700f7b372789a14ad66699
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-06-10 08:49:14 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Arranging the array/arranging-the-array.cpp
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            vector<int>pos,neg;
+            int i;
+            for(i=0;i<n;i++)
+            {
+                if(arr[i] < 0)
+                {
+                    neg.push_back(arr[i]);
+                }
+                else
+                {
+                    pos.push_back(arr[i]);
+                }
+            }
+            int k = 0;
+            for(i=0;i<neg.size();i++)
+            {
+                arr[k] = neg[i];
+                k++;
+            }
+            for(i=0;i<pos.size();i++)
+            {
+                arr[k] = pos[i];
+                k++;
+            }
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
