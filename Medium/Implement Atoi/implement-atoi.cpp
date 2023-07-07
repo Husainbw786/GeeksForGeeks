60db2987c3c03da3f67d2dd707eb8e373f5671d5
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-07-07 07:33:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implement Atoi/implement-atoi.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    /*You are required to complete this method */
+    int atoi(string str) {
+        //Your code here
+        bool minus = false;
+        int ans = 0;
+        int i;
+        int n = str.size();
+        for(i=0;i<n;i++)
+        {
+            if(str[i] >= '0' && str[i] <= '9')
+            {
+                ans = ans * 10 + (str[i] - '0');
+            }
+            else if(str[0] == '-' && !minus)
+            {
+                minus = true;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        if(minus)
+        {
+            ans = ans * -1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		cin>>s;
+		Solution ob;
+		cout<<ob.atoi(s)<<endl;
+	}
+}
+// } Driver Code Ends
