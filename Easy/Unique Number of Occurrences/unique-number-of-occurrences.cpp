9dc2da4237c827dcd762e7ea56eaf11e8d922486
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-07-13 08:14:26 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Unique Number of Occurrences/unique-number-of-occurrences.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+    public:
+    bool isFrequencyUnique(int n, int arr[])
+    {
+        
+        unordered_map<int,int>mp;
+        int i;
+        for(i=0;i<n;i++)
+        {
+            mp[arr[i]]++;
+        }
+        unordered_set<int>st;
+        for(auto x : mp)
+        {
+            if(st.find(x.second) != st.end())
+            {
+                return false;
+            }
+            else
+            {
+                st.insert(x.second);
+            }
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        bool ans=ob.isFrequencyUnique(n,arr);
+        if(ans)
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
