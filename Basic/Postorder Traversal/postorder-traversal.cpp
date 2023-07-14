88787044dffd9a830d25836c749e378f4c85b656
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-07-14 10:15:15 +0530

    Updated Solution - GfG to GitHub

--- Basic/Postorder Traversal/postorder-traversal.cpp
+++ Basic/Postorder Traversal/postorder-traversal.cpp
@@ -124,21 +124,21 @@ struct Node
 }; */
 
 //Function to return a list containing the postorder traversal of the tree.
-void traversal(Node* root,vector<int>&v)
-{
-    if(root == NULL)
+void postorder(Node* root, vector<int>&v)
     {
-        return;
+        if(root == NULL)
+        {
+            return;
+        }
+       
+        postorder(root->left,v);
+        postorder(root->right,v);
+        v.push_back(root->data);
     }
-    traversal(root->left,v);
-    traversal(root->right,v);
-    v.push_back(root->data);
-}
-
 vector <int> postOrder(Node* root)
 {
   // Your code here
-  vector<int>v;
-  traversal(root,v);
-  return v;
+      vector<int>v;
+      postorder(root,v);
+      return v;
 }
