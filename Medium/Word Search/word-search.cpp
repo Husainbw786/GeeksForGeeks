c634cc375851b04cb7345eceb9f9716ed24d1224
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-08-04 18:21:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Word Search/word-search.cpp
+++ Medium/Word Search/word-search.cpp
@@ -1,74 +1,90 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+//Initial Template for C++
+
+#include <bits/stdc++.h>
 using namespace std;
 
+
+
 // } Driver Code Ends
+//User function Template for C++
 
 class Solution {
 public:
-    void dfs(int x, int y, string s, vector<vector<char>>& board, int idx, bool& found) {
-        if (x < 0 || x >= board.size() || y < 0 || y >= board[0].size()) {
-            return;
+
+    bool dfs(int idx, int i, int j, vector<vector<char>>&board, string word)
+    {
+        if(idx == word.size())
+        {
+            return true;
         }
-        
-        if (s[idx] != board[x][y]) {
-            return;
+        if(i < 0 || j < 0 || i >= board.size() || j >= board[0].size() || board[i][j] != word[idx])
+        {
+            return false;
         }
-        
-        if (idx == s.size() - 1) {
-            found = true;
-            return;
+        if(board[i][j] == word[idx])
+        {
+            char x = board[i][j];
+            board[i][j] = '#';
+            if(dfs(idx+1,i+1,j,board,word)) return true;
+            if(dfs(idx+1,i-1,j,board,word)) return true;
+            if(dfs(idx+1,i,j+1,board,word)) return true;
+            if(dfs(idx+1,i,j-1,board,word)) return true;
+            board[i][j] = x;
+            return false;
+            
         }
+        return false;
         
-        char temp = board[x][y];
-        board[x][y] = '*';
         
-        dfs(x + 1, y, s, board, idx + 1, found);
-        dfs(x - 1, y, s, board, idx + 1, found);
-        dfs(x, y + 1, s, board, idx + 1, found);
-        dfs(x, y - 1, s, board, idx + 1, found);
-        
-        board[x][y] = temp;
     }
 
-    bool isWordExist(vector<vector<char>>& board, string word) {
+    bool wordSearch(vector<vector<char>>& board, string w) {
+        //code here
+        
         int n = board.size();
         int m = board[0].size();
-        
-        bool found = false;
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                dfs(i, j, word, board, 0, found);
-                if (found) {
-                    return true;
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(board[i][j] == w[0])
+                {
+                    if(dfs(0,i,j,board,w))
+                    {
+                        return true;
+                    }
                 }
             }
         }
-        
         return false;
     }
 };
 
 //{ Driver Code Starts.
-int main(){
-	int tc;
-	cin >> tc;
-	while(tc--){
-		int n, m;
-		cin >> n >> m;
-		vector<vector<char>>board(n, vector<char>(m, '*'));
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++)
-				cin >> board[i][j];
-		string word;
-		cin >> word;
-		Solution obj;
-		bool ans = obj.isWordExist(board, word);
-		if(ans)
-			cout << "1\n";
-		else cout << "0\n";
-	}
-	return 0;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        vector<vector<char>> mat(n,vector<char>(m));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                cin>>mat[i][j];
+            }
+        }
+        string str; cin>>str;
+        Solution ob;
+        auto ans=ob.wordSearch(mat,str);
+        cout<<ans<<"\n";
+    }
+    return 0;
 }
+
 // } Driver Code Ends
