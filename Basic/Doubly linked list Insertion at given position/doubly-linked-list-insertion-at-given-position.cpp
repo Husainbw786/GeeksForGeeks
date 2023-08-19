aa5efe31700699f051755b6ba144c3563d7a6c75
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-08-19 09:19:39 +0530

    Updated Solution - GfG to GitHub

--- Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
+++ Basic/Doubly linked list Insertion at given position/doubly-linked-list-insertion-at-given-position.cpp
@@ -79,6 +79,8 @@ int main()
 // } Driver Code Ends
 
 
+
+
 /* a Node of the doubly linked list 
 struct Node
 {
@@ -93,14 +95,17 @@ void addNode(Node *head, int pos, int data)
 {
    // Your code here
    int count = 0;
+   Node* curr = head;
    while(count < pos)
    {
-       head = head->next;
+       curr = curr->next;
        count++;
    }
-   Node* temp = new Node(data);
-   temp->next = head->next;
-   head->next = temp;
-   temp->prev = head;
+  
+  Node* temp = new Node(data);
+  temp->next = curr->next;
+  curr->next = temp;
+  temp->prev =  curr;
+  //temp->next->prev = temp;
    
 }
