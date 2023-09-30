651d51736cc3806c7d10a36bbda3fbeeb0cf8b49
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-09-30 19:09:23 +0530

    Updated Solution - GfG to GitHub

--- Easy/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
+++ Easy/Largest subarray with 0 sum/largest-subarray-with-0-sum.cpp
@@ -19,22 +19,19 @@ class Solution{
         int maxi = 0;
         for(i=0;i<n;i++)
         {
-            sum += A[i];
-            if(sum == 0)
-            {
-                maxi = i+1;
-            }
-            else
-            {
-               if(mp.find(sum) != mp.end())
-                {
-                    maxi = max(maxi,i - mp[sum]);
-                }
-                else
-                {
-                   mp[sum] = i; 
-                } 
-            }
+          sum += A[i];
+          if(sum == 0)
+          {
+              maxi = max(maxi,i+1);
+          }
+          if(mp.find(sum) != mp.end())
+          {
+              maxi = max(maxi,i - mp[sum]);
+          }
+          else
+          {
+               mp[sum] = i;
+          }
         }
         return maxi;
     }
