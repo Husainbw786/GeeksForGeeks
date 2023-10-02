fbe74fbcfb14e801a0e948966fe2ec9fad7512fd
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-10-02 09:30:15 +0530

    Updated Solution - GfG to GitHub

--- Easy/Geek Jump/geek-jump.cpp
+++ Easy/Geek Jump/geek-jump.cpp
@@ -6,19 +6,44 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    
+    int solve(int idx, vector<int>heights, vector<int>&dp)
+    {
+        if(idx == 0)
+        {
+            return 0;
+        }
+        if(dp[idx] != -1)
+        {
+            return dp[idx];
+        }
+        int one = solve(idx-1,heights,dp) + abs(heights[idx] - heights[idx-1]);
+        int second = INT_MAX;
+        if(idx > 1)
+        {
+            second = solve(idx-2,heights,dp) + abs(heights[idx] - heights[idx-2]);
+        }
+        return dp[idx] = min(one,second);
+        
+    }
+  
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
+        
+       // vector<int>dp(n,-1);
+    //    return solve(n-1,height,dp);
+        
+        
         vector<int>dp(n,0);
-        int i;
-        for(i=1;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            int left = dp[i-1] + abs(height[i-1] - height[i]);
-            int right = INT_MAX;
+            int one = dp[i-1] + abs(height[i] - height[i-1]);
+            int second = INT_MAX;
             if(i > 1)
             {
-                right = dp[i-2] + abs(height[i-2] - height[i]);
+                second = dp[i-2] + abs(height[i] - height[i-2]);
             }
-            dp[i] = min(left,right);
+            dp[i] = min(second,one);
         }
         return dp[n-1];
     }
