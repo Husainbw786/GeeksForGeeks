ccf58240bce4f958caed33885b133d099975de05
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2023-10-20 19:13:46 +0530

    Updated Solution - GfG to GitHub

--- Hard/Minimum sum partition/minimum-sum-partition.cpp
+++ Hard/Minimum sum partition/minimum-sum-partition.cpp
@@ -4,53 +4,35 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution{
-
   public:
+  
+  int solve(int i,int sum, int n, int arr[],vector<vector<int>>&dp,int total)
+  {
+      if(i == n)
+      {
+          return abs(total-2*sum);
+      }
+      if(dp[i][sum] != -1)
+      {
+          return dp[i][sum];
+      }
+      int not_take = solve(i+1,sum,n,arr,dp,total);
+      int take = solve(i+1,sum + arr[i],n,arr,dp,total);
+      return dp[i][sum] = min(take,not_take);
+  }
+  
+  
 	int minDifference(int arr[], int n)  { 
 	    // Your code goes here
-	     int totSum = 0;
-
-  for (int i = 0; i < n; i++) 
-  {
-    totSum += arr[i];
-  }
-
-  vector < vector < bool >> dp(n, vector < bool > (totSum + 1, false));
-
-  for (int i = 0; i < n; i++) 
-  {
-    dp[i][0] = true;
-  }
-
-  if (arr[0] <= totSum)
-    dp[0][totSum] = true;
-
-  for (int ind = 1; ind < n; ind++) 
-  {
-    for (int target = 1; target <= totSum; target++)
-    {
-
-      bool notTaken = dp[ind - 1][target];
-
-      bool taken = false;
-      if (arr[ind] <= target)
-        taken = dp[ind - 1][target - arr[ind]];
-
-      dp[ind][target] = notTaken || taken;
-    }
-  }
-
-  int mini = 1e9;
-  for (int i = 0; i <= totSum; i++) 
-  {
-    if (dp[n - 1][i] == true) 
-    {
-      int diff = abs(i - (totSum - i));
-      mini = min(mini, diff);
-    }
-  }
-  return mini;
+	    int total = 0;
+	    int i;
+	    for(i=0;i<n;i++)
+	    {
+	        total += arr[i];
+	    }
+	    vector<vector<int>>dp(n,vector<int>(total+1,-1));
 	    
+	    return solve(0,0,n,arr,dp,total);
 	} 
 };
 
