be13c159e62bd96f49bfbe9a4bd2dc7106ff5ccd
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-01-03 22:01:02 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Smallest window containing 0, 1 and 2/smallest-window-containing-0-1-and-2.cpp
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int smallestSubstring(string s) {
+        // Code here
+        bool zero = false,one = false,two = false;
+        int Zcount,Ocount,Tcount;
+        int ans = INT_MAX;
+        int i;
+        for(i=0;i<s.length();i++)
+        {  
+            if(s[i] == '0')
+           {
+            zero = true;
+            Zcount = i;
+           }
+           else if(s[i] == '1')
+           {
+               one = true;
+               Ocount = i;
+           }
+           
+           else if(s[i] == '2')
+           {
+               two = true;
+               Tcount = i;
+           }
+           if(one && two && zero)
+           {
+             ans = min(ans,max({Ocount,Tcount,Zcount}) - min({Ocount,Tcount,Zcount}));
+             
+           }
+          
+            
+        }
+         if(ans == INT_MAX)
+           {
+               return -1;
+           }
+        return ans + 1;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.smallestSubstring(S);
+        cout << endl;
+    }
+}
+// } Driver Code Ends
