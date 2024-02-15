1374791edb9d72224e70264a938ace28b4098a08
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-02-15 11:37:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Count all Possible Path/count-all-possible-path.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution {
+public:
+	int isPossible(vector<vector<int>>paths){
+	    // Code here
+	    int n = paths.size();
+	    int m = paths[0].size();
+	    int i,j;
+	    for(i=0;i<n;i++)
+	    {
+	        int count = 0;
+	        for(j=0;j<m;j++)
+	        {
+	            if(paths[i][j] == 1)
+	            {
+	                count++;
+	            }
+	        }
+	        if(count % 2 != 0)
+	        {
+	            return 0;
+	        }
+	    }
+	    return 1;
+	    
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<vector<int>>paths(n, vector<int>(n, 0));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++)
+				cin >> paths[i][j];
+		}
+		Solution obj;
+		int ans = obj.isPossible(paths);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
