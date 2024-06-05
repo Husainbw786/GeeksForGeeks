cd5f401b4e1997c69b5b6fa9811934f6a2cd4cfb
Husain Baghwala <89205542+Husainbw786@users.noreply.github.com>
2024-06-05 17:01:48 +0530

    Updated Solution - GfG to GitHub

--- Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
+++ Medium/Swapping pairs make sum equal/swapping-pairs-make-sum-equal.cpp
@@ -2,15 +2,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// } Driver Code Ends
-class Solution{
 
-	public:
-	int findSwapValues(int A[], int n, int B[], int m)
-	{
-        // Your code goes here
-        
-        
+// } Driver Code Ends
+class Solution {
+
+  public:
+    int findSwapValues(int A[], int n, int B[], int m) {
+   
         int i;
         int sum1 = 0;
         int sum2 = 0;
@@ -50,40 +48,29 @@ class Solution{
         }
         return -1;
 	}
- 
-
 };
 
 //{ Driver Code Starts.
 
-int main() 
-{
-   	
-   	int t;
+int main() {
+
+    int t;
     cin >> t;
-    while (t--)
-    {
-    	int n,m;
-        cin>>n>>m;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
         int a[n];
         int b[m];
-        for(int i=0;i<n;i++)
-            cin>>a[i];
-        for(int i=0;i<m;i++)
-            cin>>b[i];
-        
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+        for (int i = 0; i < m; i++)
+            cin >> b[i];
 
         Solution ob;
-        cout <<  ob.findSwapValues(a, n, b, m);
-	    cout << "\n";
-	     
+        cout << ob.findSwapValues(a, n, b, m);
+        cout << "\n";
     }
     return 0;
 }
-
-
-
-
-
 
 // } Driver Code Ends
